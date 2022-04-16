d335221c70a172d791c13c918756ccca2a9b6fa1
chibashu <rogerkensny854@gmail.com>
2022-04-17 00:16:07 +0900

    チャット表示順変更

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,7 @@
 class RoomsController < ApplicationController
   def show
     # 投稿一覧表示に利用
-    @messages = Message.includes(:user).order(id: "DESC")
+    @messages = Message.includes(:user).order(:id)
     # メッセージ投稿に利用
     @message = current_user.messages.build
   end
