6e4c6aa53660929e648ef04162533a6a427a900c
chibashu <rogerkensny854@gmail.com>
2022-04-16 18:13:19 +0900

    リロード無しのチャット機能実装

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,15 +1,28 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+// turbolinks の読み込みが終わった後にidを取得しないとエラーが出る。
+document.addEventListener('turbolinks:load', () => {
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  // js.erb 内で使用できるように変数を定義しておく
+  window.messageContainer = document.getElementById('message-container')
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
+  // 以下のプログラムが他のページで動作しないようにしておく ※「===」は厳密等価演算子
+  if (messageContainer === null) {
+    return
   }
-});
+
+  consumer.subscriptions.create("RoomChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      // サーバー側から受け取ったHTMLを一番最後に加える
+      messageContainer.insertAdjacentHTML('beforeend', data['message'])
+    }
+  })
+})
