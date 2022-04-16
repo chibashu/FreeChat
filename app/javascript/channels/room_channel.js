a500cd363af4f4dad7475fb7b2f64732c9fadede
chibashu <rogerkensny854@gmail.com>
2022-04-17 00:05:35 +0900

    ページの一番下に移動

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -3,7 +3,7 @@ import consumer from "./consumer"
 // turbolinks の読み込みが終わった後にidを取得しないとエラーが出る。
 document.addEventListener('turbolinks:load', () => {
 
-  // js.erb 内で使用できるように変数を定義しておく
+  // js.erb 内で使用できるように変数を定義
   window.messageContainer = document.getElementById('message-container')
 
   // 以下のプログラムが他のページで動作しないようにしておく ※「===」は厳密等価演算子
@@ -25,4 +25,16 @@ document.addEventListener('turbolinks:load', () => {
       messageContainer.insertAdjacentHTML('beforeend', data['message'])
     }
   })
+
+  const documentElement = document.documentElement
+
+  // js.erb 内でも使用できるように変数を定義
+  window.messageContent = document.getElementById('message_content')
+  // 一番下まで移動する関数。js.erb 内でも使用できるように変数を定義
+  window.scrollToBottom = () => {
+    window.scroll(0, documentElement.scrollHeight)
+  }
+
+  // 最初にページ一番下へ移動させる
+  scrollToBottom()
 })
